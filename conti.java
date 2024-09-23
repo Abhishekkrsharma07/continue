eac77be8d6c22398a0209a7d903774f734643032
ABHISHEK KUMAR SHARMA <151908859+Abhishekkrsharma07@users.noreply.github.com>
2024-09-23 10:36:37 +0530

    Update conti.java

--- conti.java
+++ conti.java
@@ -1,4 +1,37 @@
-public class conti{
+//TAKING INPUT FROM THE USER USING SCANNER CLASS
+
+import java.util.Scanner;
+
+public class abhi{
+
+    public static void main(String []args){
+        int i, j ,n, m;
+
+        
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.print("Enter the value of n and m : ");                                  //SIMPLE CODE IS IN THE BOTTOM
+                                                                                                            |
+        n=sc.nextInt();
+        m=sc.nextInt();
+        
+        for(i=0;i<=n;i++){
+
+            for(j=1;j<=m;j++){
+
+                if(i==2&&j==2){                                                                            |
+
+                    continue;
+
+                }
+                System.out.println(i + " " + j);
+            }
+        }                                                                                                  |
+    }
+}
+ 
+
+/*public class conti{
     public static void main(String []args){
         int i,j;
 
@@ -15,37 +48,5 @@ public class conti{
             }
         }
     }
-}
+}*/
 
-//TAKING INPUT FROM THE USER USING SCANNER CLASS
-/*
- * import java.util.Scanner;
-
-public class abhi{
-
-    public static void main(String []args){
-        int i, j ,n, m;
-
-        
-        Scanner sc=new Scanner(System.in);
-        
-        System.out.print("Enter the value of n and m : ");
-        
-        n=sc.nextInt();
-        m=sc.nextInt();
-        
-        for(i=0;i<=n;i++){
-
-            for(j=1;j<=m;j++){
-
-                if(i==2&&j==2){
-
-                    continue;
-
-                }
-                System.out.println(i + " " + j);
-            }
-        }
-    }
-}
- */
